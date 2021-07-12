26f16ee92b2c07bfeb02892d9c1902f65c8bf6e3
mrgou <rgoubet@gmail.com>
2021-07-12 09:57:47 +0200

    ivql2

--- main/ivql2.py
+++ main/ivql2.py
@@ -1,21 +1,22 @@
-import requests
 import pandas as pd
-from getpass import getuser
+import tabulate
+import requests
+import json
+import csv
+import argparse
+import time
+import os
+import configparser
+import sqlite3
+import sys
+
 from dataclasses import dataclass
 from http.client import responses
-from reportlab.lib import colors, pdfencrypt
-from reportlab.lib.pagesizes import A3, A4, landscape
-from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
-from reportlab.lib.units import mm
-from reportlab.platypus import Paragraph, SimpleDocTemplate, Table, TableStyle
-from secrets import choice
-from string import ascii_letters, digits, punctuation
-from datetime import datetime
-from tzlocal import get_localzone
+from re import match
 from urllib.parse import urlparse
+from prompt_toolkit import prompt
 
 
-# define custom exceptions and object class to handle authorization
 class AuthenticationException(Exception):
     pass
 
@@ -31,17 +32,44 @@ class session_details:
     allvaults: dict
 
 
-@dataclass
-class user:
-    firstName: str
-    lastName: str
-    userName: str
-    email: str
-    securityProfile: str
-    securityPolicy: str
-    group: str
-    activationDate: str
+class custom_df(pd.DataFrame):
+    def __init__(self, *args):
+        pd.DataFrame.__init__(self, *args)
 
+    def expand(self):
+        def expand_col(col, sep="_"):
+            df = col.apply(pd.Series)
+            if 0 in df.columns:  # this occurs for NaN rows
+                df.drop(columns=0, inplace=True)
+            mapping = {newcol: f"{col.name}{sep}{newcol}" for newcol in df.columns}
+            df.rename(mapping, axis="columns", inplace=True)
+            return df
+
+        while True:
+            processed = False
+            for col in self.columns:
+                first_val = self[col].first_valid_index()
+                if first_val != None:
+                    if type(self[col].iloc[first_val]) == list:
+                        self = self.explode(col)
+                        processed = True
+            self = self.reset_index(drop=True)
+            for col in self.columns:
+                first_val = self[col].first_valid_index()
+                if first_val != None:
+                    if type(self[col].iloc[first_val]) == dict:
+                        self = pd.concat(
+                            [self, expand_col(self[col])],
+                            axis="columns",
+                        ).drop(col, axis="columns")
+                        processed = True
+            if not processed:
+                break
+        return self
+
+    @staticmethod
+    def cjson_normalize(data):
+        return custom_df(pd.json_normalize(data))
 
 
 def authorize(vault: str, user_name: str, password: str) -> session_details:
@@ -84,6 +112,25 @@ def authorize(vault: str, user_name: str, password: str) -> session_details:
         raise
 
 
+def parse_args():
+    # Parse command line arguments and return parameters
+    parser = argparse.ArgumentParser(description="Convert CSV delimiters")
+    parser.add_argument(
+        "-i",
+        "--input",
+        default=",",
+        help="Specifies the delimiter for the CSV file (default=',')",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=";",
+        help="Name of the ouput CSV file (default='merged.csv')",
+    )
+    parser.add_argument("file", help="CSV file")
+    return parser.parse_args()
+
+
 def execute_vql(
     session: session_details,
     vql_query: str,
@@ -96,12 +143,14 @@ def execute_vql(
             strLimit = ""
         else:
             strLimit = " LIMIT " + str(limit)
-        http_params = {"q": vql_query + strLimit}
+        payload = {"q": vql_query + strLimit}
+        http_params = {}
         if tokenize:
             http_params["tokenize"] = str(tokenize)
-        r = requests.get(
+        r = requests.post(
             session.mainvault[2] + "/query",
             params=http_params,
+            data=payload,
             headers={"Authorization": session.sessionId},
         )
         response = r.json()
@@ -133,64 +182,12 @@ def execute_vql(
         return {"error": "Connection Error"}
 
 
-def df_to_pdf(data, pdf_file):
-    """
-    Generates a PDF report from a Pandas DataFrame object
-    """
+def main():
+    args = parse_args()  # get command line arguments
 
-    def addPageNumber(
-        canvas, doc
-    ):  # this subfunction will be called to add page numbers
-        page_num = canvas.getPageNumber()
-        text = "Page %s" % page_num
-        canvas.drawCentredString(200 * mm, 20 * mm, text)
 
-    # build paragraph styles
-    styles = getSampleStyleSheet()
-    styleH = styles["Heading1"]
-    styleC = ParagraphStyle("small", parent=styles["Normal"], fontSize=8)
-    enc_pw = "".join(
-        choice(ascii_letters + digits + punctuation) for i in range(12)
-    )  # build a strong 12-char password
-    enc = pdfencrypt.StandardEncryption(
-        "", ownerPassword=enc_pw, canModify=0
-    )  # set encryption
-    data.fillna("", inplace=True)
-    table_data = data.values.tolist()
-    # convert the dataframe to a list of list,
-    # enclose the content of cell values in a Paragraph object in order to apply word wrapping
-    formatted_data = [data.columns.values.tolist()] + [
-        [Paragraph(str(cell), styleC) for cell in row] for row in table_data
-    ]
-    table_style = [
-        ("GRID", (0, 0), (-1, -1), 0.25, colors.black),  # full grid on the whole table
-        ("FONTNAME", (0, 0), (-1, 0), "Helvetica-Bold"),  # first row in bold
-        ("FONTSIZE", (0, 0), (-1, -1), 8),
-        ("VALIGN", (0, 0), (-1, -1), "TOP"),  # cell content aligned to the top
-    ]
-    pdf_table = Table(
-        data=formatted_data,
-        repeatRows=1,
-        style=TableStyle(table_style),
-        spaceBefore=24,
-        hAlign="LEFT",
-    )
-    title = Paragraph(
-        "Report generated on "
-        + datetime.now(tz=get_localzone()).strftime("%d %b %Y %H:%M:%S %Z %z"),
-        styleH,
-    )
-    doc = SimpleDocTemplate(
-        pdf_file,
-        pagesize=landscape(A3),
-        leftMargin=36,
-        rightMargin=36,
-        title="Vault Audit Trail Report",
-        author=getuser(),
-        encrypt=enc,
-    )
-    element = []
-    element.append(title)
-    element.append(pdf_table)
-    doc.build(element, onFirstPage=addPageNumber, onLaterPages=addPageNumber)
-    print("Exported %s audit records to PDF" % str(len(data.index)))
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        sys.exit("Bye!")
